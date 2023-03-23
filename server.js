f950651423b90a0bf3656404e0b49f7dd1a17d4d
sharplight226 <sharplight226@gmail.com>
2023-03-23 16:08:29 +0200

    comment

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
-
+//////////////////////////////
 function initial() {
   Role.estimatedDocumentCount((err, count) => {
     if (!err && count === 0) {
