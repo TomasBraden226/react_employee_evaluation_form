22e54b4499539d184575190a7fbac7f287a664c4
sharplight226 <sharplight226@gmail.com>
2023-03-24 06:38:43 +0200

    back to pre

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -102,8 +102,7 @@ exports.signin = (req, res) => {
         id: user._id,
         username: user.username,
         email: user.email,
-        roles: authorities,
-        token: token
+        roles: authorities
       });
     });
 };
