73fdd1a45b14c3f8be21f6f3471e4cdc89dcc7a6
TomasBraden226 <sharplight226@gmail.com>
2023-03-24 09:52:41 +0900

    Fixing errors of frontend

--- client/src/components/EvaluateForm.js
+++ client/src/components/EvaluateForm.js
@@ -1,9 +1,51 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class EvaluateForm extends Component {
-  render() {
-    return (
-      <div>EvaluateForm</div>
-    )
+import Button from '@mui/material/Button';
+
+const styles = {
+  primaryBackgroundColor: {
+    backgroundColor: 'rgb(103, 58, 183)', 
+  },
+  formHeader: {
+    padding: '30px 15px'
+  },
+  formCard: {
+    backgroundColor: '#fff',
+    width: '100%', 
+    padding: '22px 24px',
+    borderRadius: '.5em',
+  },
+  formHeaderTopCard: {
+    backgroundColor: '#fff', 
+    borderRadius: '0 0 .5em .5em',
+  },
+  formSubCard: {
+    padding: '22px 24px',
+  },
+  marBtm20: {
+    marginBottom: '20px'
+  },
+  marTop20: {
+    marginTop: '20px'
+  },
+  formInput: {
+    width: '100%',
+  },
+  formHeaderBar: {
+    width: '100%', 
+    backgroundColor: 'rgb(103, 58, 183)', 
+    borderRadius: '.5em .5em 0 0', 
+    padding: '.3em', 
+    color: '#fff'
   }
+}
+
+const steps = ['Select campaign settings', 'Create an ad group', 'Criteria'];
+
+export default function EvaluateForm() {
+  return (
+    <div>
+      <Button variant="contained">Hello World</Button>
+    </div>
+  )
 }
